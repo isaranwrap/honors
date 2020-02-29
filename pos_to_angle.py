581cb0e259e5b10708e790c50bc56db9509e7e24
Ishan Saran <ishansaran65@gmail.com>
2020-02-29 14:31:05 +0000

    removing all the functions so I can clean up and organize a little

--- pos_to_angle.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import h5py 
-
-d = h5py.File('/home/ishan/princeton_data'
--- summerstuff/plot_connections.py
+++
@@ -1,8 +0,0 @@
-#Returns none, plots to current working axis
-def plot_connections(xpos, ypos,num_points = 25):
-    plt.cla()
-    plt.xlim(0,148)
-    plt.ylim(148,0)
-    [plt.plot([xpos[c[0]],xpos[c[1]]],[ypos[c[0]],ypos[c[1]]]) for c in connections[:num_points]];
-    
-  
